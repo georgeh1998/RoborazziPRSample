537b42270aa24fb71e5a56866296919028b6d4a6
georgeh1998 <gouta.gorogoro@gmail.com>
2024-04-29 22:41:08 +0900

    chore

---
+++ app/src/main/java/com/github/georgeh1998/roborazziprsample/MainScreen.kt
@@ -0,0 +1,18 @@
+package com.github.georgeh1998.roborazziprsample
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        repeat(10) {
+            Text(text = "Text $it")
+        }
+    }
+}
