b5eddd9821915b5da5e583978c0456e64d93fcc6
georgeh1998 <gouta.gorogoro@gmail.com>
2024-05-02 15:12:04 +0900

    push from testtest

--- app/src/main/java/com/github/georgeh1998/roborazziprsample/MainScreen.kt
+++ app/src/main/java/com/github/georgeh1998/roborazziprsample/MainScreen.kt
@@ -11,7 +11,7 @@ fun MainScreen() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        repeat(11) {
+        repeat(14) {
             Text(text = "Text $it")
         }
     }
