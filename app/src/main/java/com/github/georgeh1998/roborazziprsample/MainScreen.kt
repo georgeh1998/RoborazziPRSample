fb9e9e6fec1bdea661baf43154864d0734ec9a23
georgeh1998 <gouta.gorogoro@gmail.com>
2024-05-26 16:06:23 +0900

    test1

--- app/src/main/java/com/github/georgeh1998/roborazziprsample/MainScreen.kt
+++ app/src/main/java/com/github/georgeh1998/roborazziprsample/MainScreen.kt
@@ -11,7 +11,7 @@ fun MainScreen() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        repeat(15) {
+        repeat(10) {
             Text(text = "Text $it")
         }
     }
